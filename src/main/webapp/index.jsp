309fb62fd3e261884b38e1287754f70d1321f005
Ole Martin Sleveland Hidle <omhidle@student.uia.no>
2020-10-21 03:43:29 +0200

    Initial commit

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+
+
+</head>
+
+
+<body>
+
+<h1> <style>style="..."</style> Welcome!!  </h1>
+
+<h2>        Hello Rowing Enthusiasts! <3  </h2>
+
+<form action="/LogIn/" method="get">
+    <input type="text" name="name" placeholder="Skriv navnet ditt her...">
+    <button style="color: blue" style="animation-direction: normal" type="submit">Log in</button>
+</form>
+
+
+
+</body>
+</html>
---
+++ src/main/webapp/servlets/LogIn.java
@@ -0,0 +1,27 @@
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.https.*;
+
+@WebServlet("/olema/IdeaProjects/RoKlubbProsjekt/src/main/webapp/servlets")
+public class LogIn extends HttpsServlet {
+
+    private String message;
+
+
+    public void doGet(HttpsServletRequest request, HttpsServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        out.print(name);
+    }
+
+    public void destroy() {
+        // do nothing.
+    }
+}
