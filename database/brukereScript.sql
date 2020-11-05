c0099e5b07659550ba70534fce88e82c6deeb1d3
Ole Martin Hidle <omhidle@uia.no>
2020-11-05 16:08:24 +0100

    Database update

---
+++ database/brukereScript.sql
@@ -0,0 +1,24 @@
+create table Tilganger
+(
+    Type varchar(10) not null
+        primary key
+);
+
+create table Brukere
+(
+    BrukerID    int(32) auto_increment,
+    Epost       varchar(32) not null,
+    Passord     varchar(20) not null,
+    Rettigheter varchar(10) not null,
+    constraint Brukere_BrukerID_uindex
+        unique (BrukerID),
+    constraint Brukere_Epost_uindex
+        unique (Epost),
+    constraint Brukere_Passord_uindex
+        unique (Passord),
+    constraint Brukere_Tilganger_Type_fk
+        foreign key (Rettigheter) references Tilganger (Type)
+);
+
+alter table Brukere
+    add primary key (BrukerID);
