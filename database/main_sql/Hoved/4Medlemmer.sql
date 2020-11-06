89789e0795ffa9ea55b17140a57bc31fc98c3c02
Ole Martin Hidle <omhidle@uia.no>
2020-11-06 11:48:05 +0100

    Updatex2

--- database/main_sql/Hoved/4Medlemmer.sql
+++ database/main_sql/Hoved/4Medlemmer.sql
@@ -5,12 +5,19 @@ create table Medlemmer
     Medlems_Rolle varchar(20) not null,
     Medlems_Navn  varchar(20) not null,
     Fødselsdato   varchar(20) not null,
+    `Rank`        int(10)     null,
+    Score         int(10)     null,
     Kjønns_Gruppe varchar(20) not null,
     Alders_Gruppe varchar(20) not null,
-    Vekt          int(100)    null,
+    constraint Medlemmer_Medlems_Navn_uindex
+        unique (Medlems_Navn),
+    constraint Medlemmer_Rank_uindex
+        unique (`Rank`),
     constraint Medlemmer_Klasser_Aldersgruppe_fk
         foreign key (Alders_Gruppe) references Klasser (Aldersgruppe),
     constraint Medlemmer_Klasser_Klasse_Kjønn_fk
         foreign key (Kjønns_Gruppe) references Klasser (Klasse_Kjønn)
 );
+
+
 
