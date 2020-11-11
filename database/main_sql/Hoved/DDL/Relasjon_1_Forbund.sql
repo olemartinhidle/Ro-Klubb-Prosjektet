d354a281719828a8ebc0e3a9c50ef98ec3b0e056
Ole Martin Hidle <omhidle@uia.no>
2020-11-11 23:49:04 +0100

    Login ferdig, sammt DB script.

---
+++ database/main_sql/Hoved/DDL/Relasjon_1_Forbund.sql
@@ -0,0 +1,6 @@
+create table RoForbundDB.Forbund
+(
+    ForbundsID    int auto_increment
+        primary key,
+    Forbunds_Navn varchar(20) default 'Norges Roforbund' not null
+);
--- src/main/com/servlets/LogUt.java
+++ src/main/com/servlets/LogUt.java
@@ -12,7 +12,7 @@ import java.io.PrintWriter;
 public class LogUt extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
 
